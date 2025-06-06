8795a989dafccf9b7efd0378b440358d6ef66737
Angel Velasquez <pcisavel@upc.edu.pe>
2025-06-06 06:04:38 -0500

    feat(orm): update database schema with health records.

--- shared/infrastructure/database.py
+++ shared/infrastructure/database.py
@@ -5,8 +5,6 @@ This module sets up the SQLite database connection and initializes the database.
 
 from peewee import SqliteDatabase
 
-
-
 # Define the database connection
 db = SqliteDatabase('smart_band.db')
 
@@ -15,6 +13,7 @@ def init_db() -> None:
     """Initialize the database and create tables if they do not exist."""
     db.connect()
     from iam.infrastructure.models import Device
-    db.create_tables([Device], safe=True)  # Add your models here
+    from health.infrastructure.models import HealthRecord
+    db.create_tables([Device, HealthRecord], safe=True)  # Add your models here
     print("Database initialized.")
     db.close()
